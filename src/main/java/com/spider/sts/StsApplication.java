55002a8f8ffb04efc853ca05c9e8f804935b7a31
spidergh <zhanghongtao93@163.com>
2020-03-18 21:47:58 +0800

    create project

---
+++ src/main/java/com/spider/sts/StsApplication.java
@@ -0,0 +1,13 @@
+package com.spider.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spider/sts/StsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spider.sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
